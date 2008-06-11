6f1a033492ce06467ffe2246e9d142169577bf44
Justin Bogner <jbogner@yottayotta.com>
2008-06-10 20:03:56 -0600

    a better approach to comments

--- functions.el
+++ functions.el
@@ -1,20 +1,20 @@
-(defun comment-line ()
+(defun comment-or-uncomment-line ()
   "Comment the line that the point is on."
   (interactive)
   (save-excursion
     (end-of-line)
     (let ((end (point)))
       (back-to-indentation)
-      (comment-region (point) end))))
+      (comment-or-uncomment-region (point) end))))
 
-(defun uncomment-line ()
-  "Uncomment the line that the point is on."
-  (interactive)
-  (save-excursion
-    (end-of-line)
-    (let ((end (point)))
-      (beginning-of-line)
-      (uncomment-region (point) end))))
+(defun comment-or-uncomment-dwim (arg)
+  "If transient-mark-mode is t and the mark is active,
+comment-or-uncomment-region, otherwise, comment the line that the
+point is on, or if arg is set, comment that many lines."
+  (interactive "*P")
+  (if (and mark-active transient-mark-mode)
+      (comment-or-uncomment-region (region-beginning) (region-end) arg)
+    (call-interactively 'comment-or-uncomment-line)))
 
 (defun fit-windows ()
   "Fit all of the windows as to minimize unused space"
--- keys.el
+++ keys.el
@@ -1,20 +1,18 @@
 ;; Compilation --- prefix \C-cc to prompt for a compile command
 (setq-default compilation-read-command nil)
-(global-set-key "\C-cb" (lambda (pfx)
-                          (interactive "p")
-                          (setenv "buffer" (buffer-file-name))
-                          (call-interactively 'compile)))
+(global-set-key [?\C-c b] (lambda (pfx)
+                            (interactive "p")
+                            (setenv "buffer" (buffer-file-name))
+                            (call-interactively 'compile)))
 
 ;; dabbrev-completion is more bash-like than dabbrev-expand
-(global-set-key "\M-/" (lambda ()
-                         (interactive)
-                         (dabbrev-completion)
-                         (fit-windows)))
+(global-set-key [?\M-/] (lambda ()
+                          (interactive)
+                          (dabbrev-completion)
+                          (fit-windows)))
 
 ;; TODO: think of a better shortcut here
-(global-set-key "\M-]" 'fit-windows)
+(global-set-key [?\M-\]] 'fit-windows)
 
-(global-set-key [?\C-\<] 'comment-region)
-(global-set-key [?\C-\>] 'uncomment-region)
-(global-set-key [?\C-\,] 'comment-line)
-(global-set-key [?\C-\.] 'uncomment-line)
+(global-set-key [?\C-\;] 'comment-or-uncomment-dwim)
+
