bf5d31abbdea4c2a7bf95e88f10c241ab8e996ee
Justin Bogner <jbogner@yottayotta.com>
2008-06-13 10:22:41 -0600

    auto-tag magic

--- functions.el
+++ functions.el
@@ -20,3 +20,26 @@ point is on, or if arg is set, comment that many lines."
   "Fit all of the windows as to minimize unused space"
   (interactive)
   (walk-windows 'shrink-window-if-larger-than-buffer))
+
+(defun vc-root ()
+  "Find the root directory of the current buffer's version control"
+  (vc-call root (buffer-file-name)))
+
+(defun create-tags (dir ext)
+  "Create a tags file in 'dir' for files with extension
+'ext'. Does nothing if such a tags file already exists"
+  (setq tags-file-name (format "%sTAGS_%s" dir ext))
+  (when (and (not (file-exists-p tags-file-name))
+             (file-accessible-directory-p dir))
+    (shell-command
+     (format "cd %s && find . -type f -name '*.%s' | etags -o %s -"
+             dir ext (file-name-nondirectory tags-file-name)))))
+
+(defun auto-tag ()
+  "Automatically create and/or select a tags table"
+  (interactive)
+  (let* ((file (buffer-file-name))
+         (dir default-directory)
+         (ext (file-name-extension file)))
+    (when (vc-registered file) (setq dir (vc-root)))
+    (create-tags dir ext)))
