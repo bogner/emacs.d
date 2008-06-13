43c935404944480a036fec430d12147e97f845b1
Justin Bogner <jbogner@yottayotta.com>
2008-06-13 10:21:49 -0600

    keybinding syntax confuses, use the one that works

--- keys.el
+++ keys.el
@@ -1,9 +1,9 @@
 ;; Compilation --- prefix \C-cc to prompt for a compile command
 (setq-default compilation-read-command nil)
-(global-set-key [?\C-c b] (lambda (pfx)
-                            (interactive "p")
-                            (setenv "buffer" (buffer-file-name))
-                            (call-interactively 'compile)))
+(global-set-key "\C-cb" (lambda (pfx)
+                          (interactive "p")
+                          (setenv "buffer" (buffer-file-name))
+                          (call-interactively 'compile)))
 
 ;; dabbrev-completion is more bash-like than dabbrev-expand
 (global-set-key [?\M-/] (lambda ()
