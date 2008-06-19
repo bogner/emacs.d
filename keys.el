d4aa24aaaea816a48659547b4a2f048d9c7c80ae
Justin Bogner <jbogner@yottayotta.com>
2008-06-18 21:44:03 -0600

    kbd syntax

--- keys.el
+++ keys.el
@@ -1,27 +1,27 @@
 ;; Compilation --- prefix \C-cc to prompt for a compile command
 (setq-default compilation-read-command nil)
-(global-set-key "\C-cb" (lambda (pfx)
-                          (interactive "p")
-                          (setenv "buffer" (buffer-file-name))
-                          (call-interactively 'compile)))
+(global-set-key (kbd "C-c b") (lambda (pfx)
+                                (interactive "p")
+                                (setenv "buffer" (buffer-file-name))
+                                (call-interactively 'compile)))
 
 ;; dabbrev-completion is more bash-like than dabbrev-expand
-(global-set-key [?\M-/] (lambda ()
-                          (interactive)
-                          (dabbrev-completion)
-                          (fit-windows)))
+(global-set-key (kbd "M-/") (lambda ()
+                              (interactive)
+                              (dabbrev-completion)
+                              (fit-windows)))
 
 ;; TODO: think of a better shortcut here
-(global-set-key [?\M-\]] 'fit-windows)
+(global-set-key (kbd "M-]") 'fit-windows)
 
-(global-set-key [?\C-\;] 'comment-or-uncomment-dwim)
+(global-set-key (kbd "C-\\") 'comment-or-uncomment-dwim)
 
-; C-z in a window system is confusing and useless
-(global-set-key [?\C-z] (lambda ()
-                          (interactive)
-                          (if window-system
-                              (message "i'm afraid i can't do that dave")
-                            (suspend-frame))))
+;; C-z in a window system is confusing and useless
+(global-set-key (kbd "C-z") (lambda ()
+                              (interactive)
+                              (if window-system
+                                  (message "i'm afraid i can't do that dave")
+                                (suspend-frame))))
 
 ;; find the file or url at the point, if possible
 (ffap-bindings)
