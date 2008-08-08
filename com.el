a924b0351f38bd1f96ae8c7a4dd62917424695b7
Justin Bogner <mail@justinbogner.com>
2008-08-07 22:34:02 -0600

    rcirc tweaks

--- com.el
+++ com.el
@@ -5,8 +5,15 @@
                             :channels ("#haskell" "#xmonad"))))
 (when (file-exists-p "rcirc-auth.el") (load "rcirc-auth"))
 
+;; Show buffers with unread messages in mode line
+(setq rcirc-track-minor-mode t)
+
 ;; Logging
 (setq rcirc-log-flag t)
 
 ;; Keep 1M worth of 80 character lines in memory
 (setq rcirc-buffer-maximum-lines 13107)
+
+;; We would like to clear the unread marker explicitly
+(define-key rcirc-mode-map (kbd "C-c C-f")
+  (lambda () (interactive) (rcirc-clear-unread (current-buffer))))
