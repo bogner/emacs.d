3ffc29e2ead909265acedd9757dcf3755ad25493
Justin Bogner <jbogner@yottayotta.com>
2008-11-14 11:26:36 -0700

    4 space indent default, 8 for actual tabs

--- behaviour.el
+++ behaviour.el
@@ -81,10 +81,7 @@
 
 ;; Global indentation rules
 (setq-default indent-tabs-mode nil
-              standard-indent 2)
-;; set tab stops to every 4 spaces (mostly for asm-mode)
-(setq-default tab-stop-list
-      '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
+              standard-indent 4)
 
 ;; Ensure newline at EOF
 (setq require-final-newline t)
